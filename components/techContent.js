35fc1d58cde0d4a9eaef6d1aeadafd0ef515392c
ruben wilson <105153564+ruben-wilson@users.noreply.github.com>
2022-11-30 13:29:48 +0000

    added in new route aboutMe

--- components/techContent.js
+++ components/techContent.js
@@ -16,10 +16,4 @@ const techIcons = {
   }
 }
 
-// const content = [<p className="text-5xl mx-2">< DiJsBadge /></p>, <p className="text-5xl mx-2">< DiNodejsSmall /></p>, <p className="text-5xl mx-2">< DiMongodb /></p>, <Image className="mx-2" src={expressIcon} draggable="false"></Image>, <Image className="mx-2" src={nodemonIcon} draggable="false"></Image>, <Image className="max-w-[50px] max-h-[50px] mx-2" src={cypressIcon} draggable="false"></Image>]
-// const contentReefRunner = [<p className="text-5xl mx-2">< DiJsBadge /></p>, <p className="text-5xl mx-2">< DiHtml5 /></p>, <Image className="max-w-[65px] max-h-[65px] mx-2" src={phaserIcon} draggable="false"></Image>]
-// const contentAceBook = [<p className="text-5xl mx-2">< DiJsBadge /></p>, <p className="text-5xl mx-2">< DiNodejsSmall /></p>, <p className="text-5xl mx-2">< DiMongodb /></p>, <Image className="mx-2" src={expressIcon} draggable="false"></Image>, <Image className="mx-2" src={nodemonIcon} draggable="false"></Image>, <Image className="max-w-[50px] max-h-[50px] mx-2" src={cypressIcon} draggable="false"></Image>]
-// const contentmakerBnb = [<p className="text-5xl mx-2">< DiRuby /></p>, <Image className="min-w-[50px] max-h-[60px] mx-2" src={sinatraIcon} draggable="false"></Image>, <p className="text-5xl mx-2">< DiPostgresql /></p>, <p className="text-5xl mx-2">< DiBootstrap /></p>, <Image className="max-w-[50px] max-h-[50px] mx-2" src={cypressIcon} draggable="false"></Image>]
-// const contentChitter = [<p className="text-5xl mx-2">< DiRuby /></p>, <Image className="min-w-[50px] max-h-[60px] mx-2" src={sinatraIcon} draggable="false"></Image>, <p className="text-5xl mx-2">< DiPostgresql /></p>, <Image className="max-w-[50px] max-h-[50px] mx-2" src={cypressIcon} draggable="false"></Image>]
-
 module.exports = techIcons
