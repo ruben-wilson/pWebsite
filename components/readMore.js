09088c76c69dfa1f3b8166e86c9300bc0d3477a3
ruben wilson <105153564+ruben-wilson@users.noreply.github.com>
2022-12-08 19:43:54 +0000

    added in text content for readMore page

--- components/readMore.js
+++ components/readMore.js
@@ -1,4 +1,4 @@
-const readMore = ({preview, content}) =>{
+const readMore = ({ preview, content, content2 }) =>{
   return (
     <div className="collapse">
       <input type="checkbox" className="peer" />
@@ -6,7 +6,8 @@ const readMore = ({preview, content}) =>{
         <p className="text-gray-400">{preview} <span className="text-gray-50 font-bold">click to read more ...</span></p>
       </div>
       <div className="collapse-content ">
-        <p className="text-gray-400">{content}</p>
+        <p className="text-gray-400">{content}</p><br></br>
+        <p className="text-gray-400">{content2}</p>
       </div>
     </div>
   )
