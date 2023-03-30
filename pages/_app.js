d7b62085678d7b96704f86a86f3a339b1ed3d33b
ruben wilson <rwilson_connell@yahoo.com>
2023-03-30 15:38:10 +0100

    tidying up project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return ( 
   <Layout>
     <Component {...pageProps} />
@@ -9,4 +9,4 @@ function MyApp({ Component, pageProps }) {
   )
 }
 
-export default MyApp
+module.exports = MyApp;
