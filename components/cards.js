3da4df74a37c09e2960efbe59799b140ad56c0af
ruben wilson <rwilson_connell@yahoo.com>
2023-03-30 15:10:14 +0100

    updated cards

--- components/cards.js
+++ components/cards.js
@@ -11,6 +11,7 @@ import makerBnbLogo from "../public/logo/makersbnbLogo.png"
 import aceBookLogo from "../public/logo/aceBookLogo.png"
 import multiNoteLogo from "../public/logo/multiNoteLogo.png"
 import reefRunnerLogo from "../public/logo/reefrunnerLogo.png"
+import bankAccountLogo from "../public/logo/bankAccountLogo.jpg"
 
 
 import makerBnb from "../public/screenshots/makerBnB.png"
@@ -18,6 +19,7 @@ import aceBook from "../public/screenshots/aceBook.png"
 import multiNote from "../public/screenshots/multiNote.png"
 import reefRunner from "../public/screenshots/reefrunnerSc.png"
 import chitter from "../public/screenshots/chitter.png"
+import BankAccount from "../public/screenshots/BankAccount.png"
 
 import Card from "./card"
 
@@ -39,6 +41,7 @@ const cards = ({ xValue }) => {
       <motion.div ref={carousel} className="cursor-pointer overflow-hidden mt-[2rem] mb-4 max-h-[30rem] max-w-[50rem] rounded-xl">
         <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} animate={{ x: xValue.movement }} transition={{ duration: 0.4 }} className="flex" id='cards-myWork'>
           <Card name="ReefRunner" image1={reefRunnerLogo} image2={reefRunner} repo="https://github.com/ruben-wilson/team-sea-urchins" website="https://resonant-ganache-a523ff.netlify.app/" content={contents.reefRunner} description={contents.reefRunner.description} isHosted="true" />
+          <Card name="BankAccount" image1={bankAccountLogo} image2={BankAccount} content={contents.bankAccount} description={contents.bankAccount.description} repo="https://github.com/ruben-wilson/BankAccount-java" />
           <Card name="MultiNote" image1={multiNoteLogo} image2={multiNote} repo="https://github.com/ruben-wilson/Multi-Note-front-end" website="https://jazzy-taffy-d5f222.netlify.app"  content={contents.multiNote} description={contents.multiNote.description} isHosted="true" />
           <Card name="AceBook" image1={aceBookLogo} image2={aceBook} content={contents.aceBook} description={contents.aceBook.description} />
           <Card name="Makers BnB" image1={makerBnbLogo} image2={makerBnb} content={contents.makersBnb} description={contents.makersBnb.description} />
--- components/techContent.js
+++ components/techContent.js
@@ -3,13 +3,19 @@ import nodemonIcon from '../public/icons/nodemonIcon.png'
 import phaserIcon from '../public/icons/phaserIcon.png'
 import cypressIcon from '../public/icons/cypressIcon.svg'
 import sinatraIcon from '../public/icons/sinatraIcon.svg'
-import { DiNodejsSmall, DiMongodb, DiJsBadge, DiHtml5, DiPostgresql, DiBootstrap, DiRuby } from 'react-icons/di'
+import { DiNodejsSmall, DiMongodb, DiJsBadge, DiHtml5, DiPostgresql, DiBootstrap, DiRuby, DiJava } from 'react-icons/di'
+import { SiSpringboot, SiMysql } from 'react-icons/si'
+
 import Image from "next/image";
 
 const techIcons = {
     multiNote: {
        icons: [<p className="text-5xl mx-2">< DiJsBadge /></p>, <p className="text-5xl mx-2">< DiNodejsSmall /></p>, <p className="text-5xl mx-2">< DiMongodb /></p>, <Image className="mx-2" src={expressIcon} draggable="false"></Image>, <Image className="mx-2" src={nodemonIcon} draggable="false"></Image>, <Image className="max-w-[50px] max-h-[50px] mx-2" src={cypressIcon} draggable="false"></Image>],
        description: "this is a project designed to help people keep track of daily tasks and goals. Built with vanilla js and html. I built this application to explore javascript in the front and learn more about the DOM."
+    },
+    bankAccount: {
+      icons: [<p className="text-5xl mx-2">< DiJava /></p>, <p className="text-5xl mx-2">< SiSpringboot /></p>, <p className="text-5xl mx-2">< SiMysql /></p>],
+      description: "A BankAccount is a springboot application to simulate an bank account, it uses microservice architecture and follows OOP. I built it in Springboot with SpringBootJpa and Mysql."
     },
     reefRunner: {
       icons: [<p className="text-5xl mx-2">< DiJsBadge /></p>, <p className="text-5xl mx-2">< DiHtml5 /></p>, <Image className="max-w-[65px] max-h-[65px] mx-2" src={phaserIcon} draggable="false"></Image>],
